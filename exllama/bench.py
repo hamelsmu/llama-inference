02e9479f15f8259a49c7584de6d049d6e8824f65
hamelsmu <hamel.husain@gmail.com>
2023-07-24 05:35:32 +0000

    save benchmarks

--- exllama/bench.py
+++ exllama/bench.py
@@ -1,37 +1,38 @@
-#see https://github.com/huggingface/text-generation-inference
-import sys
-import argparse
+import openai
+import os, sys, time, argparse
+if not os.getenv('OPENAI_API_KEY'):
+    raise ValueError("Must set environment variable OPENAI_API_KEY")
 sys.path.append('../common/')
-import time
 import pandas as pd
-from text_generation import Client
 from questions import questions
 from tqdm import tqdm
 
-
+url = 'http://0.0.0.0:5001/v1'
+openai.api_base = url
+# Parse the command-line arguments
 # Define the argument parser
 parser = argparse.ArgumentParser(description='Run LLM inference requests and save to a csv.')
 parser.add_argument('--filename', type=str, required=True, help='Path to the output CSV file.')
 parser.add_argument('--note', type=str, required=True, help='Note to add to the rows of the file.')
-
-# Parse the command-line arguments
 args = parser.parse_args()
 
-def generate_text_and_save_results(filename):
 
-    client = Client("http://127.0.0.1:8081", timeout=500)
+def generate_text_and_save_results(filename):
     counter = 1
     responses = []
 
     for q in tqdm(questions):
         start = time.perf_counter()
-        result = client.generate(q, max_new_tokens=200, best_of=1)
+        result =openai.Completion.create(model='TheBloke_Llama-2-7B-GPTQ',
+                                        prompt="Say this is a test",
+                                        max_tokens=200,
+                                        temperature=0)
         request_time = time.perf_counter() - start
         if counter >= 2: # allow for a warmup
-            responses.append({'tok_count': result.details.generated_tokens, 
+            responses.append({'tok_count': result.usage.completion_tokens, 
                             'time': request_time,
                             'question': q, 
-                            'answer': result.generated_text, 
+                            'answer': result.choices[0].text, 
                             'note': args.note})
         counter +=1
 
--- tgi/start_server.sh
+++ tgi/start_server.sh
@@ -6,12 +6,11 @@ then
   exit 1
 fi
 
-model=meta-llama/Llama-2-7b-hf
 volume=$PWD/data 
 
 docker run --gpus all \
--e HUGGING_FACE_HUB_TOKEN=$HUGGING_FACE_HUB_TOKEN \
+ -e HUGGING_FACE_HUB_TOKEN=$HUGGING_FACE_HUB_TOKEN \
+ -e GPTQ_BITS=4 -e GPTQ_GROUPSIZE=128 \
  --shm-size 5g -p 8081:80 \
  -v $volume:/data ghcr.io/huggingface/text-generation-inference \
- --model-id $model \
  --max-best-of 1 "$@"
