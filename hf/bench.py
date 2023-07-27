c63e8525d0aa151f252cb5dda6ecc520ebe4dfee
Hamel Husain <hamel.husain@gmail.com>
2023-07-27 12:28:18 -0700

    add notes

---
+++ hf/bench.py
@@ -0,0 +1,5 @@
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
