eaa1c525a34b3c1f1b1df9c56aa0b30f6bfdb8c1
Iván Baldo <ibaldo@netlabs.com.uy>
2023-12-12 17:48:09 -0300

    New bench-bt_fa.py identical to bench.py ATM.

---
+++ hf/bench-bt_fa.py
@@ -0,0 +1,37 @@
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import time
+import sys
+sys.path.append('../common/')
+from questions import questions
+import pandas as pd
+
+model_id = "meta-llama/Llama-2-7b-hf"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id)
+model.to("cuda")
+
+def predict(prompt:str):
+    start_time = time.perf_counter()
+    inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
+    generated_ids = model.generate(**inputs, max_length=200)
+    output = tokenizer.batch_decode(generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+    request_time = time.perf_counter() - start_time
+    return {'tok_count': generated_ids.shape[1],
+        'time': request_time,
+        'question': prompt,
+        'answer': output,
+        'note': ''}
+
+
+if __name__ == '__main__':
+    counter = 1
+    responses = []
+
+    for q in questions:
+        if counter >= 2: responses.append(predict(q))
+        counter += 1
+
+    df = pd.DataFrame(responses)
+    df.to_csv('bench.csv', index=False)
+
